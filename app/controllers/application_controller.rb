92ddf31f87b79d21d89381b7ee9a57f2e1baf3b2
joshthecomposer <jdwisecomposer@gmail.com>
2023-07-10 20:36:22 -0500

    session visibility and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :set_current_user
+  
+    helper_method :current_user, :user_signed_in?
+  
+    private
+  
+    def set_current_user
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+  
+    def current_user
+      @current_user
+    end
+  
+    def user_signed_in?
+      current_user.present?
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  delete '/logout', to: 'sessions#destroy', as: :logout
 end
